a1a024828cc3f257b49f8e7b21d45de3208e3318
Raphaël Cohen <raphael.cohen.utt@gmail.com>
2015-05-15 14:09:01 +0200

    Added the setup.py file

---
+++ setup.py
@@ -0,0 +1,56 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+
+setup(
+    name='pyjection',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='Pyjection is a lightweight python dependency injection library',
+
+    # The project's main homepage.
+    url='https://github.com/Darkheir/pyjection',
+
+    # Author details
+    author='Raphael Cohen',
+    author_email='raphael.cohen.utt@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Natural Language :: English',
+    ],
+
+    # What does your project relate to?
+    keywords='dependency injection dependency-injection development',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'test': ['coverage'],
+    },
+
+)
